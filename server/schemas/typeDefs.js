1cc14e90a8011e234174cb0717e7cb9c5e7dfd86
demiapollo <apollodemirel67@gmail.com>
2023-07-12 11:53:03 -0700

    resolvers & typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,42 +1,55 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        bookCount: Int
-        savedBooks: [Book]
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
-    type Query {
-        me: User
-    }
-    input BookInput {
-        bookId: ID!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: BookInput): User
-        removeBook(bookId: ID!): User
-    }
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: String!
+    user: User!
+  }
+  type Query {
+    me: User
+  }
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
 `;
 
 module.exports = typeDefs;
